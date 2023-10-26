4442d593ad836fc637164b125f6996cc054dfbe2
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-26 13:40:11 +0100

    Fix: Format with prettier

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,26 +1,32 @@
-export async function shopifyFetch({ query, variables }: {query: string, variables: string}) {
-  const endpoint = process.env.SHOPIFY_STORE_DOMAIN ?? '';
-  const key = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN ?? '';
+export async function shopifyFetch({
+  query,
+  variables,
+}: {
+  query: string
+  variables: string
+}) {
+  const endpoint = process.env.SHOPIFY_STORE_DOMAIN ?? ''
+  const key = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN ?? ''
 
   try {
     const result = await fetch(endpoint, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'X-Shopify-Storefront-Access-Token': key
+        'X-Shopify-Storefront-Access-Token': key,
       },
-      body: { query, variables } && JSON.stringify({ query, variables })
-    });
+      body: { query, variables } && JSON.stringify({ query, variables }),
+    })
 
     return {
       status: result.status,
-      body: await result.json()
-    };
+      body: await result.json(),
+    }
   } catch (error) {
-    console.error('Error:', error);
+    console.error('Error:', error)
     return {
       status: 500,
-      error: 'Error receiving data'
-    };
+      error: 'Error receiving data',
+    }
   }
 }
