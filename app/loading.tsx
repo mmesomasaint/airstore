09000d17f19720bddaade94d9722fc466ec6c888
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-26 14:24:48 +0100

    Feat: Create loading file

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className='fixed inset-0 bg-black/40 flex justify-center items-center'>
+      <p className="text-xl font-semibold text-black">Loading...</p>
+    </div>
+  )
+}
