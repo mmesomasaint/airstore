ef2a5f37db5ff48c15bf8597051c5a852c6fdfc0
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-27 17:41:47 +0100

    Feat: Running fetch should depend on endpoint & key being defined

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -5,23 +5,25 @@ export async function shopifyFetch({
   query: string
   variables: string
 }) {
-  const endpoint = process.env.SHOPIFY_STORE_DOMAIN ?? ''
-  const key = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN ?? ''
+  const endpoint = process.env.SHOPIFY_STORE_DOMAIN
+  const key = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN
 
   try {
-    const result = await fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Shopify-Storefront-Access-Token': key,
-      },
-      body: { query, variables } && JSON.stringify({ query, variables }),
-    })
+    if (endpoint && key) {
+      const result = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Shopify-Storefront-Access-Token': key,
+        },
+        body: { query, variables } && JSON.stringify({ query, variables }),
+      })
 
-    return {
-      status: result.status,
-      body: await result.json(),
-    }
+      return {
+        status: result.status,
+        body: await result.json(),
+      }
+    } else throw new Error('Endpoint or key is undefined')
   } catch (error) {
     console.error('Error:', error)
     return {
