39591087884ac5db0d724c3869c7675ffe52ed25
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-28 23:02:03 +0100

    Fix: Start writing the search fxn

--- lib/search.ts
+++ lib/search.ts
@@ -1,3 +1,26 @@
-export default function Search(title: string, filter: object) { 
+import { shopifyFetch } from "./fetch";
 
+export default function Search(title: string, filter: object) {
+  const searchQuery = `title:*${title}*`
+  const data = shopifyFetch({
+    query: `
+    query AllProducts($first: Int = 250, $searchText: String) {
+      products(first: $first, query: title:*$searchText*) {
+        id
+        title
+        handle
+        description
+        descriptionHtml
+        featuredImage
+        images(first: 1) {
+          url
+        }
+      }
+    }
+    `,
+    variables: `{
+      first: 250,
+      searchText: searchQuery
+    }`
+  })
 }
