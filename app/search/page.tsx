bff11e0d0f966c16d630f8db5bda7deb54bdecc9
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-29 22:20:57 +0100

    Fix: Format with prettier

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -12,121 +12,147 @@ import Range from '@/theme/components/range'
 import Header from '@/theme/components/header'
 
 export default function Home() {
-  const { searchText, setSearchText, searchResults, searchHandler, filter, categories, resetCategories, setCategory, setCondition, setPaymentGateway, setPrice } = useSearch()
+  const {
+    searchText,
+    searchResults,
+    filter,
+    categories,
+    setSearchText,
+    searchHandler,
+    resetCategories,
+    setCategory,
+    setCondition,
+    setPaymentGateway,
+    setPrice,
+  } = useSearch()
 
   return (
     <main className='min-h-screen flex flex-col'>
-      <Header searchText={searchText} setSearchText={setSearchText} filter={filter} categories={categories} resetCategories={resetCategories} setCategory={setCategory} searchClick={searchHandler} />
+      <Header
+        searchText={searchText}
+        setSearchText={setSearchText}
+        filter={filter}
+        categories={categories}
+        resetCategories={resetCategories}
+        setCategory={setCategory}
+        searchClick={searchHandler}
+      />
       <div className='bg-gray-100/70 px-7 py-4 min-h-full grow grid grid-cols-12 gap-5 place-items-start'>
         <div className='col-span-3 h-fit w-full'>
-      <div className='h-fit w-full flex flex-col gap-5 bg-white rounded-xl border border-store-outline-faded-max p-5'>
-        <TextMid>Filters</TextMid>
-        <HR>
-          <Accordion title='Categories' defaultOpen>
-            <CheckBox
-              check={filter.categories.airpod}
-              setCheck={(value: boolean) => setCategory(value, 'airpod')}
-            >
-              Airpods
-            </CheckBox>
-            <CheckBox
-              check={filter.categories.iPhone}
-              setCheck={(value: boolean) => setCategory(value, 'iPhone')}
-            >
-              iPhone
-            </CheckBox>
-            <CheckBox
-              check={filter.categories.iPad}
-              setCheck={(value: boolean) => setCategory(value, 'iPad')}
-            >
-              iPad
-            </CheckBox>
-            <CheckBox
-              check={filter.categories.macbook}
-              setCheck={(value: boolean) => setCategory(value, 'macbook')}
-            >
-              Macbook
-            </CheckBox>
-            <CheckBox
-              check={filter.categories.iWatch}
-              setCheck={(value: boolean) => setCategory(value, 'iWatch')}
-            >
-              iWatch
-            </CheckBox>
-          </Accordion>
-        </HR>
-        <HR>
-          <Accordion title='Condition'>
-            <CheckBox
-              check={filter.conditions.newStuff}
-              setCheck={(value: boolean) => setCondition(value, 'newStuff')}
-            >
-              New Stuff
-            </CheckBox>
-            <CheckBox
-              check={filter.conditions.fairlyUsed}
-              setCheck={(value: boolean) => setCondition(value, 'fairlyUsed')}
-            >
-              Fairly Used
-            </CheckBox>
-            <CheckBox
-              check={filter.conditions.secondHand}
-              setCheck={(value: boolean) => setCondition(value, 'secondHand')}
-            >
-              Second Hand
-            </CheckBox>
-          </Accordion>
-        </HR>
-        <HR>
-          <Accordion title='Price'>
-            <Range
-              ranges={[
-                [500, 1000],
-                [1000, 1500],
-                [1500, 2000],
-                [2000, 2500],
-                [2500, 3000],
-              ]}
-              min={filter.price.min}
-              max={filter.price.max}
-              setMin={(value: number) => setPrice(value, 'min')}
-              setMax={(value: number) => setPrice(value, 'max')}
-            />
-          </Accordion>
-        </HR>
-        <Accordion title='Payment' defaultOpen>
-          <CheckBox
-            check={filter.paymentGateways.cashOnDelivery}
-            setCheck={(value: boolean) =>
-              setPaymentGateway(value, 'cashOnDelivery')
-            }
-          >
-            Cash on Delivery
-          </CheckBox>
-          <CheckBox
-            check={filter.paymentGateways.prepaid}
-            setCheck={(value: boolean) => setPaymentGateway(value, 'prepaid')}
-          >
-            Prepaid
-          </CheckBox>
-          <CheckBox
-            check={filter.paymentGateways.iStoreCoupon}
-            setCheck={(value: boolean) =>
-              setPaymentGateway(value, 'iStoreCoupon')
-            }
-          >
-            iStore Coupon
-          </CheckBox>
-          <CheckBox
-            check={filter.paymentGateways.binancePay}
-            setCheck={(value: boolean) =>
-              setPaymentGateway(value, 'binancePay')
-            }
-          >
-            Binance Pay
-          </CheckBox>
-        </Accordion>
-      </div>
+          <div className='h-fit w-full flex flex-col gap-5 bg-white rounded-xl border border-store-outline-faded-max p-5'>
+            <TextMid>Filters</TextMid>
+            <HR>
+              <Accordion title='Categories' defaultOpen>
+                <CheckBox
+                  check={filter.categories.airpod}
+                  setCheck={(value: boolean) => setCategory(value, 'airpod')}
+                >
+                  Airpods
+                </CheckBox>
+                <CheckBox
+                  check={filter.categories.iPhone}
+                  setCheck={(value: boolean) => setCategory(value, 'iPhone')}
+                >
+                  iPhone
+                </CheckBox>
+                <CheckBox
+                  check={filter.categories.iPad}
+                  setCheck={(value: boolean) => setCategory(value, 'iPad')}
+                >
+                  iPad
+                </CheckBox>
+                <CheckBox
+                  check={filter.categories.macbook}
+                  setCheck={(value: boolean) => setCategory(value, 'macbook')}
+                >
+                  Macbook
+                </CheckBox>
+                <CheckBox
+                  check={filter.categories.iWatch}
+                  setCheck={(value: boolean) => setCategory(value, 'iWatch')}
+                >
+                  iWatch
+                </CheckBox>
+              </Accordion>
+            </HR>
+            <HR>
+              <Accordion title='Condition'>
+                <CheckBox
+                  check={filter.conditions.newStuff}
+                  setCheck={(value: boolean) => setCondition(value, 'newStuff')}
+                >
+                  New Stuff
+                </CheckBox>
+                <CheckBox
+                  check={filter.conditions.fairlyUsed}
+                  setCheck={(value: boolean) =>
+                    setCondition(value, 'fairlyUsed')
+                  }
+                >
+                  Fairly Used
+                </CheckBox>
+                <CheckBox
+                  check={filter.conditions.secondHand}
+                  setCheck={(value: boolean) =>
+                    setCondition(value, 'secondHand')
+                  }
+                >
+                  Second Hand
+                </CheckBox>
+              </Accordion>
+            </HR>
+            <HR>
+              <Accordion title='Price'>
+                <Range
+                  ranges={[
+                    [500, 1000],
+                    [1000, 1500],
+                    [1500, 2000],
+                    [2000, 2500],
+                    [2500, 3000],
+                  ]}
+                  min={filter.price.min}
+                  max={filter.price.max}
+                  setMin={(value: number) => setPrice(value, 'min')}
+                  setMax={(value: number) => setPrice(value, 'max')}
+                />
+              </Accordion>
+            </HR>
+            <Accordion title='Payment' defaultOpen>
+              <CheckBox
+                check={filter.paymentGateways.cashOnDelivery}
+                setCheck={(value: boolean) =>
+                  setPaymentGateway(value, 'cashOnDelivery')
+                }
+              >
+                Cash on Delivery
+              </CheckBox>
+              <CheckBox
+                check={filter.paymentGateways.prepaid}
+                setCheck={(value: boolean) =>
+                  setPaymentGateway(value, 'prepaid')
+                }
+              >
+                Prepaid
+              </CheckBox>
+              <CheckBox
+                check={filter.paymentGateways.iStoreCoupon}
+                setCheck={(value: boolean) =>
+                  setPaymentGateway(value, 'iStoreCoupon')
+                }
+              >
+                iStore Coupon
+              </CheckBox>
+              <CheckBox
+                check={filter.paymentGateways.binancePay}
+                setCheck={(value: boolean) =>
+                  setPaymentGateway(value, 'binancePay')
+                }
+              >
+                Binance Pay
+              </CheckBox>
+            </Accordion>
+          </div>
         </div>
         <div className='col-span-9 gap-5 flex flex-col w-full'>
           <div className='flex justify-between items-center gap-10'>
