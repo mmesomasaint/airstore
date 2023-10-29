7be0fe8f9b618fd736877ac029f98a460b1b4242
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-29 15:30:38 +0100

    Feat: Rename the store title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'iStore',
+  title: 'Airstore',
   description: 'Apple gadgets online retail store',
 }
 
