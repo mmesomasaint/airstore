f7b366ac8288d5213da7a9e504679a178eb6a47d
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-30 16:20:20 +0100

    Fix: Format with prettier

--- lib/cleanProduct.ts
+++ lib/cleanProduct.ts
@@ -16,83 +16,83 @@ type Variant = {
 
 type QueryMiniProduct = {
   id: string
-        title: string
-        handle: string
-        totalInventory: number
-        featuredImage: {
-          url: string
-        }
-        priceRange: {
-          minVariantPrice: {
-            amount: string
-          }
-        }
-        compareAtPriceRange: {
-          maxVariantPrice: {
-            amount: string
-          }
-        }
-        options: {
-          name: string
-          values: string
-        }[]
+  title: string
+  handle: string
+  totalInventory: number
+  featuredImage: {
+    url: string
+  }
+  priceRange: {
+    minVariantPrice: {
+      amount: string
+    }
+  }
+  compareAtPriceRange: {
+    maxVariantPrice: {
+      amount: string
+    }
+  }
+  options: {
+    name: string
+    values: string
+  }[]
 }
 
 type QueryFullProduct = {
+  id: string
+  title: string
+  description: string
+  priceRange: {
+    minVariantPrice: {
+      amount: string
+      currencyCode: string
+    }
+  }
+  compareAtPriceRange: {
+    maxVariantPrice: {
+      amount: string
+    }
+  }
+  featuredImage: {
     id: string
-    title: string
-    description: string
-    priceRange: {
-      minVariantPrice: {
-        amount: string
-        currencyCode: string
+    originalSrc: string
+    altText: string
+  }
+  variants: {
+    edges: {
+      node: Variant
+    }
+  }
+  publishedAt: string
+  updatedAt: string
+  images: {
+    edges: {
+      node: {
+        id: string
+        originalSrc: string
+        altText: string
       }
     }
-    compareAtPriceRange: {
-      maxVariantPrice: {
-        amount: string
+  }
+  options: {
+    edges: {
+      node: {
+        id: string
+        name: string
+        values: string
       }
     }
-    featuredImage: {
-      id: string
-      originalSrc: string
-      altText: string
-    }
-    variants: {
-      edges: {
-        node: Variant
+  }
+  metafields: {
+    edges: {
+      node: {
+        id: string
+        key: string
+        value: string
+        namespace: string
       }
     }
-    publishedAt: string
-    updatedAt: string
-    images: {
-      edges: {
-        node: {
-          id: string
-          originalSrc: string
-          altText: string
-        }
-      }
-    }
-    options: {
-      edges: {
-        node: {
-          id: string
-          name: string
-          values: string
-        }
-      }
-    }
-    metafields: {
-      edges: {
-        node: {
-          id: string
-          key: string
-          value: string
-          namespace: string
-        }
-      }
-    }
+  }
 }
 
 export function cleanProduct() {
@@ -100,7 +100,15 @@ export function cleanProduct() {
 }
 
 export function cleanMiniProduct(queryResult: QueryMiniProduct) {
-  const { id, title, handle, featuredImage, priceRange, compareAtPriceRange, options } = queryResult
+  const {
+    id,
+    title,
+    handle,
+    featuredImage,
+    priceRange,
+    compareAtPriceRange,
+    options,
+  } = queryResult
   const { url } = featuredImage
   const { minVariantPrice } = priceRange
   const { maxVariantPrice } = compareAtPriceRange
@@ -112,6 +120,8 @@ export function cleanMiniProduct(queryResult: QueryMiniProduct) {
     src: url,
     price: parseInt(minVariantPrice.amount).toLocaleString('en-US'),
     discount: parseInt(maxVariantPrice.amount).toLocaleString('en-US'),
-    colors: options.filter(option => option.name === 'Color').map(option => option.values),
+    colors: options
+      .filter((option) => option.name === 'Color')
+      .map((option) => option.values),
   }
 }
