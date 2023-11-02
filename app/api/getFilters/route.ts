a95f9199b34a95b7df9676da4dde2bcfbaa00ece
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-02 07:10:17 +0100

    Feat: Create the getFilter fxn

---
+++ app/api/getFilters/route.ts
@@ -0,0 +1,42 @@
+import { shopifyFetch } from '@/lib/fetch'
+import { NextRequest } from 'next/server'
+
+const query = `
+query GetFilters($first: Int) {
+  products(first: $first) {
+    nodes {
+        id
+        createdAt
+        priceRange {
+          minVariantPrice {
+            amount
+          }
+        }
+        options {
+          name
+          values
+        }
+        collections (first: $first) {
+          nodes {
+            title
+          }
+        }
+    }
+  }
+}
+`
+
+const LIMIT = 20
+
+export async function GET(req: NextRequest) {
+  const variables = {
+    first: LIMIT,
+  }
+  const { status, body } = await shopifyFetch({ query, variables })
+
+  if (status === 200) {
+    return Response.json({ status: 200, body: body.data.products.nodes })
+  } else {
+    return Response.json({ status: 500, message: 'Error receiving data' })
+  }
+}
