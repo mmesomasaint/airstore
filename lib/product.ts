498e1458c6b26246e692c7a564a904de975368a1
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-03 10:34:01 +0100

    Feat: Create the mini product interface

---
+++ lib/product.ts
@@ -0,0 +1,11 @@
+export interface MiniProduct {
+  title: string
+  variants: string[]
+  src: string
+  price: number
+  discount: number
+  colors: string[]
+  rating: number
+  amountSold: number
+  category?: string
+}
