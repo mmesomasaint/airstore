20bdb2de6177b4e6969654523a34fe6cd177f9f1
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-03 18:11:13 +0100

    Feat: Create the collection filter file

---
+++ app/collection/filter.ts
@@ -0,0 +1,37 @@
+'use server'
+
+import { Filter } from "@/lib/filter"
+
+const query = `
+query CollectionProducts ($limit: Int!, $handle: String!, $priceMax: Int!, $priceMin: Int!){
+	collection (handle:$handle) {
+    id
+    handle
+    title
+    products (first: $limit, filters: [{price: {max: $priceMax, min: $priceMin}, variantOption: {name: "", value: ""}}]) {
+      nodes {
+        id
+        createdAt
+        priceRange {
+          minVariantPrice {
+            amount
+          }
+        }
+        options {
+          name
+          values
+        }
+        collections (first: 24) {
+          nodes {
+            title
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+export default async function FilterCollection(handle: string, filter: Filter) {
+  
+}
