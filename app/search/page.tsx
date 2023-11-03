ef39787c018e165fb7ee5accff70006d89221c76
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-03 16:16:22 +0100

    Fix: Integrate the filter panel with search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import DropDown from '@/theme/components/dropdown'
+import Filter from '@/theme/components/filter'
 import { TextMid, TextTiny, TextXSmall } from '@/theme/elements/text'
 import { MiniProduct } from '@/lib/product'
 import { VCard } from '@/theme/components/product/card'
@@ -42,91 +43,7 @@ export default function Home() {
       />
       <div className='bg-gray-100/70 px-7 py-4 min-h-full grow grid grid-cols-12 gap-5 place-items-start'>
         <div className='col-span-3 h-fit w-full'>
-          <div className='h-fit w-full flex flex-col gap-5 bg-white rounded-xl border border-store-outline-faded-max p-5'>
-            <TextMid>Filters</TextMid>
-            {loadingFilter ? (
-              <div className='flex justify-center items-center w-full'>
-                <TextXSmall faded>Loading...</TextXSmall>
-              </div>
-            ) : (
-              <>
-                {filter.categories && (
-                  <HR>
-                    <Accordion title='Categories' defaultOpen>
-                      {Object.keys(filter.categories).map((category) => (
-                        <CheckBox
-                          key={category}
-                          check={filter.categories[category]}
-                          setCheck={(value: boolean) =>
-                            setCategory(value, category)
-                          }
-                        >
-                          {category}
-                        </CheckBox>
-                      ))}
-                    </Accordion>
-                  </HR>
-                )}
-                {filter.colors && (
-                  <HR>
-                    <Accordion title='Colors' defaultOpen>
-                      <div className='grow grid grid-cols-2 gap-x-5 gap-y-3'>
-                        {Object.keys(filter.colors).map((color) => (
-                          <CheckBox
-                            key={color}
-                            check={filter.colors[color]}
-                            setCheck={(value: boolean) =>
-                              setColor(value, color)
-                            }
-                          >
-                            {color}
-                          </CheckBox>
-                        ))}
-                      </div>
-                    </Accordion>
-                  </HR>
-                )}
-                {filter.dateAdded && (
-                  <HR>
-                    <Accordion title='Date Added' defaultOpen>
-                      {Object.keys(filter.dateAdded).map((date) => (
-                        <CheckBox
-                          key={date}
-                          check={filter.dateAdded[date]}
-                          setCheck={(value: boolean) =>
-                            setDateAdded(value, date)
-                          }
-                        >
-                          {date}
-                        </CheckBox>
-                      ))}
-                    </Accordion>
-                  </HR>
-                )}
-                {filter.price.tooMax > 0 && (
-                  <Accordion title='Price' defaultOpen>
-                    <Range
-                      ranges={(() => {
-                        const by2 = filter.price.tooMax / 2
-                        const by4 = filter.price.tooMax / 4
-
-                        return [
-                          [0, by4],
-                          [by4, by4 + by4],
-                          [by4 + by4, by2 + by4],
-                          [by2 + by4, filter.price.tooMax],
-                        ]
-                      })()}
-                      min={filter.price.min}
-                      max={filter.price.max}
-                      setMin={(value: number) => setPrice(value, 'min')}
-                      setMax={(value: number) => setPrice(value, 'max')}
-                    />
-                  </Accordion>
-                )}
-              </>
-            )}
-          </div>
+          <Filter filter={filter} pending={loadingFilter} setCategory={setCategory} setPrice={setPrice} setColor={setColor} setDateAdded={setDateAdded} />
         </div>
         <div className='col-span-9 gap-5 flex flex-col w-full'>
           <div className='flex justify-between items-center gap-10'>
