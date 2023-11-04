20ca163d36178d894f512afd09fb5fb363139699
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-04 19:38:24 +0100

    Fix: Delete text lines

--- app/api/get/product/route.ts
+++ app/api/get/product/route.ts
@@ -14,7 +14,6 @@ export async function GET(Request: NextRequest) {
   const { status, body } = await shopifyFetch({ query, variables })
 
   if (status === 200) {
-    console.log('body: ', body)
     const product = cleanProduct(body.data?.product)
     return Response.json({ status: 200, body: product })
   } else {
