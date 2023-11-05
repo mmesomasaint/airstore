1d00c6ff0bc94d860805a2e7de39f618ef0168d9
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-05 15:05:23 +0100

    Fix: Format with prettier

--- theme/components/editAmount.tsx
+++ theme/components/editAmount.tsx
@@ -1,4 +1,4 @@
-import { TextBase } from "../elements/text";
+import { TextBase } from '../elements/text'
 
 export default function EditAmount({
   value,
@@ -13,7 +13,10 @@ export default function EditAmount({
     <div
       className={`flex justify-start items-center ${full ? 'w-full' : 'w-1/3'}`}
     >
-      <button className='self-stretch px-4 bg-store-pri text-white rounded-l-full' onClick={() => setValue?.(value - 1)}>
+      <button
+        className='self-stretch px-4 bg-store-pri text-white rounded-l-full'
+        onClick={() => setValue?.(value - 1)}
+      >
         <TextBase>-</TextBase>
       </button>
       <input
@@ -22,7 +25,10 @@ export default function EditAmount({
         className={`grow w-full p-2 text-center placeholder:text-store-faded placeholder:text-sm placeholder:font-semibold text-store-faded font-semibold text-sm border border-store-outline-faded-max focus:outline-none focus:border-store-pri`}
         onChange={(e) => setValue?.(parseInt(e.target.value))}
       />
-      <button className='self-stretch px-4 bg-store-pri text-white rounded-r-full' onClick={() => setValue?.(value + 1)}>
+      <button
+        className='self-stretch px-4 bg-store-pri text-white rounded-r-full'
+        onClick={() => setValue?.(value + 1)}
+      >
         <TextBase>+</TextBase>
       </button>
     </div>
