2b96f3ab3422baaf538d63e77fa193fb080588e8
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-05 16:20:20 +0100

    Fix: Format with prettier

--- app/collection/[cid]/product/[pid]/page.tsx
+++ app/collection/[cid]/product/[pid]/page.tsx
@@ -263,10 +263,17 @@ function ProductPanel({ product }: { product: FullProduct }) {
         <HR>
           <div className='flex flex-col gap-2'>
             <TextXSmall>Set Quantity</TextXSmall>
-            <EditAmount value={amount} max={variant?.quantityAvailable ?? amount} setValue={setAmount} full />
+            <EditAmount
+              value={amount}
+              max={variant?.quantityAvailable ?? amount}
+              setValue={setAmount}
+              full
+            />
             <div className='flex justify-start items-center gap-1'>
               <TextTiny faded>Only</TextTiny>
-              <TextTiny primary>{variant?.quantityAvailable ?? 0} items</TextTiny>
+              <TextTiny primary>
+                {variant?.quantityAvailable ?? 0} items
+              </TextTiny>
               <TextTiny faded>left</TextTiny>
             </div>
           </div>
