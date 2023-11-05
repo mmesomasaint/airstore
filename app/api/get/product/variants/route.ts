4c2d527adee326f6f3cfb5d51a2d9cd081d41b9c
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-05 07:53:33 +0100

    Feat: Write the route POST fxn

---
+++ app/api/get/product/variants/route.ts
@@ -0,0 +1,22 @@
+import { shopifyFetch } from '@/lib/fetch'
+import { query, cleanProductWithVariant } from './utils'
+import { NextRequest } from 'next/server'
+
+export async function GET(Request: NextRequest) {
+  const searchParams = Request.nextUrl.searchParams
+  const handle = searchParams.get('handle')
+  const selectedOptions = Request.json()
+
+  const variables = {
+    handle,
+    selectedOptions,
+  }
+  const { status, body } = await shopifyFetch({ query, variables })
+
+  if (status === 200) {
+    const product = cleanProductWithVariant(body.data?.product)
+    return Response.json({ status: 200, body: product })
+  } else {
+    return Response.json({ status: 500, message: 'Error receiving data' })
+  }
+}
