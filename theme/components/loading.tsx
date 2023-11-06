f951ac0320687c2a0bd9b60b3d442d5a0f216125
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-06 07:41:29 +0100

    Feat: Create the loading component

---
+++ theme/components/loading.tsx
@@ -0,0 +1,9 @@
+import { TextXSmall } from "../elements/text";
+
+export default function Loading() {
+  return (
+    <div className='flex justify-center items-center'>
+      <TextXSmall faded>Loading...</TextXSmall>
+    </div>
+  )
+}
