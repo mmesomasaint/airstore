1ac9c955bc31c57e18fd7641c2d7f355ab630d2b
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-09 09:59:31 +0100

    Fix: Format with prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,10 +16,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
         <CartContext.Provider value={{ cartId: null }}>
-        {children}
+          {children}
         </CartContext.Provider>
       </body>
     </html>
