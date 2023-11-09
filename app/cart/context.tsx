48fa4b5a084825b9057f02aacfd51c0ef4edb6da
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-09 09:53:44 +0100

    Feat: Create the cart hook

--- app/cart/context.tsx
+++ app/cart/context.tsx
@@ -1,9 +1,11 @@
-import {createContext} from 'react'
+import {createContext, useContext} from 'react'
 
 type CartContextType = {
   cartId: string | null
 }
 
 const CartContext = createContext<CartContextType>({cartId: null})
+
+export const useCart = () => useContext(CartContext)
 
 export default CartContext
