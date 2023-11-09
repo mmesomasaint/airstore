f1590ce6c2184cbb13c64f822de01ff8f6340ecb
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-09 09:54:17 +0100

    Fix: Format with prettier

--- app/cart/context.tsx
+++ app/cart/context.tsx
@@ -1,10 +1,10 @@
-import {createContext, useContext} from 'react'
+import { createContext, useContext } from 'react'
 
 type CartContextType = {
   cartId: string | null
 }
 
-const CartContext = createContext<CartContextType>({cartId: null})
+const CartContext = createContext<CartContextType>({ cartId: null })
 
 export const useCart = () => useContext(CartContext)
 
