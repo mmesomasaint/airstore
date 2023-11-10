46fe226e2a446106d50465081a33f766d1840af4
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-10 14:57:29 +0100

    Fix: Remove useless lines

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,12 +11,6 @@ export const metadata: Metadata = {
   description: 'Apple gadgets online retail store',
 }
 
-let cartId = cookies.get('cart_id')
-if (!cartId) {
-  cartId = ''
-  cookies.set('cart_id', cartId, { expires: 1 })
-}
-
 export default function RootLayout({
   children,
 }: {
