d3ec0e31a3147ee5af7473cbfdfc3922f83d2cca
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-10 15:17:41 +0100

    Feat: Create the a proper cart provider

--- app/cart.tsx
+++ app/cart.tsx
@@ -1,6 +1,8 @@
 'use client'
 
-import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import cookies from 'js-cookie'
+import Loading from '@/theme/components/loading'
 
 type CartContextType = {
   cartId: string | null
@@ -11,9 +13,34 @@ const CartContext = createContext<CartContextType>({ cartId: null })
 export const useCart = () => useContext(CartContext)
 
 export default function CartProvider({children}: {children: React.ReactNode}) {
+  const [loading, setLoading] = useState<boolean>(true)
+  const [cartId, setCartId] = useState<string | null>(cookies.get('cart_id') ?? null)
+
+  useEffect(() => {
+    if (!cartId) {
+    fetch('/api/cart', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.body) {
+          setCartId(data.body)
+          cookies.set('cart_id', data.body)
+        }
+      })
+      .catch((e) => console.error(e))
+      .finally(() => setLoading(false))
+    }
+
+    setLoading(false)
+  }, [])
+
   return (
-    <CartContext.Provider value={{cartId: null}}>
-      {children}
+    <CartContext.Provider value={{cartId}}>
+      {loading ? <Loading /> : children}
     </CartContext.Provider>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import cookies from 'js-cookie'
 import CartProvider from './cart'
 import './globals.css'
 
