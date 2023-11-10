ecea7d8cb6ddf0a6477ec3f0ac9abb4bc7f00ede
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-10 15:17:51 +0100

    Fix: Format with prettier

--- app/cart.tsx
+++ app/cart.tsx
@@ -12,34 +12,40 @@ const CartContext = createContext<CartContextType>({ cartId: null })
 
 export const useCart = () => useContext(CartContext)
 
-export default function CartProvider({children}: {children: React.ReactNode}) {
+export default function CartProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   const [loading, setLoading] = useState<boolean>(true)
-  const [cartId, setCartId] = useState<string | null>(cookies.get('cart_id') ?? null)
+  const [cartId, setCartId] = useState<string | null>(
+    cookies.get('cart_id') ?? null
+  )
 
   useEffect(() => {
     if (!cartId) {
-    fetch('/api/cart', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.body) {
-          setCartId(data.body)
-          cookies.set('cart_id', data.body)
-        }
+      fetch('/api/cart', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
       })
-      .catch((e) => console.error(e))
-      .finally(() => setLoading(false))
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.body) {
+            setCartId(data.body)
+            cookies.set('cart_id', data.body)
+          }
+        })
+        .catch((e) => console.error(e))
+        .finally(() => setLoading(false))
     }
 
     setLoading(false)
   }, [])
 
   return (
-    <CartContext.Provider value={{cartId}}>
+    <CartContext.Provider value={{ cartId }}>
       {loading ? <Loading /> : children}
     </CartContext.Provider>
   )
