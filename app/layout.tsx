0ba78bdd67a7d04ee9f87a3f4d6437adc2234f04
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-10 14:57:04 +0100

    Fix: Format with prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <CartProvider>
-          {children}
-        </CartProvider>
+        <CartProvider>{children}</CartProvider>
       </body>
     </html>
   )
