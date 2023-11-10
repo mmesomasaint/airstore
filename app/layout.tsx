eae7947d1282f8ac70e17ca40764095a0833c446
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-10 14:56:50 +0100

    Fix: Rename context file to cart

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import cookies from 'js-cookie'
-import CartProvider from './context'
+import CartProvider from './cart'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
