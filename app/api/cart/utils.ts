1275b0a284c59422b72156a716dbe9ab03ceaf6e
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-10 17:15:57 +0100

    Feat: Create the cart api util

---
+++ app/api/cart/utils.ts
@@ -0,0 +1,62 @@
+export function generateCartInput() {
+  return ({
+    "input": {
+      "attributes": [
+        {
+          "key": "",
+          "value": ""
+        }
+      ],
+      "buyerIdentity": {
+        "countryCode": "",
+        "customerAccessToken": "",
+        "deliveryAddressPreferences": [
+          {
+            "customerAddressId": "",
+            "deliveryAddress": {
+              "address1": "",
+              "address2": "",
+              "city": "",
+              "company": "",
+              "country": "",
+              "firstName": "",
+              "lastName": "",
+              "phone": "",
+              "province": "",
+              "zip": ""
+            }
+          }
+        ],
+        "email": "",
+        "phone": "",
+        "walletPreferences": [
+          ""
+        ]
+      },
+      "discountCodes": [
+        ""
+      ],
+      "lines": [
+        {
+          "attributes": [
+            {
+              "key": "",
+              "value": ""
+            }
+          ],
+          "merchandiseId": "",
+          "quantity": 1,
+          "sellingPlanId": ""
+        }
+      ],
+      "metafields": [
+        {
+          "key": "",
+          "type": "",
+          "value": ""
+        }
+      ],
+      "note": ""
+    }
+  })
+}
