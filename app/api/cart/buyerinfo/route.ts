4f59d5605a205dd1c1943caa89340bc87dab02ef
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-11 21:32:54 +0100

    Fix: Format with prettier

--- app/api/cart/buyerinfo/route.ts
+++ app/api/cart/buyerinfo/route.ts
@@ -1,18 +1,21 @@
 import { shopifyFetch } from '@/lib/fetch'
-import {NextRequest} from 'next/server'
+import { NextRequest } from 'next/server'
 import { UPDATE_CUSTOMER_QUERY } from '../utils'
 
 export default async function POST(Request: NextRequest) {
   const searchParams = Request.nextUrl.searchParams
   const cartId = searchParams.get('cartId')
-  const {buyerIdentity} = await Request.json()
+  const { buyerIdentity } = await Request.json()
 
   const variables = {
     cartId,
-    buyerIdentity
+    buyerIdentity,
   }
-  const {status, body} = await shopifyFetch({query: UPDATE_CUSTOMER_QUERY, variables})
+  const { status, body } = await shopifyFetch({
+    query: UPDATE_CUSTOMER_QUERY,
+    variables,
+  })
 
-  if (status === 200) return Response.json({status, body: body.data.cart})
-  else return Response.json({status: 500, message: 'Error receiving data'})
+  if (status === 200) return Response.json({ status, body: body.data.cart })
+  else return Response.json({ status: 500, message: 'Error receiving data' })
 }
