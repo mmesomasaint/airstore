7c3db6d733f3ce56742c869e1e76541565d30284
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-11 12:55:36 +0100

    Fix: Redesign the cart full api

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -1,8 +1,15 @@
 import { NextRequest } from 'next/server'
+import { RETRIEVE_CART_QUERY, cleanFullCartResult } from './utils'
+import { shopifyFetch } from '@/lib/fetch'
 
 export async function GET(Request: NextRequest) {
   const searchParams = Request.nextUrl.searchParams
   const cartId = searchParams.get('cartId')
 
-  
+  const variables = {cartId}
+  const {status, body} = await shopifyFetch({query: RETRIEVE_CART_QUERY, variables})
+
+  if (status === 200) {
+    return Response.json({status: 200, body: cleanFullCartResult(body.data?.cart)})
+  } else return Response.json({status: 500, message: 'Error fetching data'})
 }
