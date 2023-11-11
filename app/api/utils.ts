a5aef4706a256ba4dc46d2b4b6087ef34fba8104
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-11 22:14:33 +0100

    Fix: Adjust the utils page

--- app/api/utils.ts
+++ app/api/utils.ts
@@ -1,5 +1,5 @@
 import { CollectionFilter, Filter } from '@/lib/filter'
-import { CartLine, FilterQueryResult, FullCartQueryResult, Merchandise, MiniCartQueryResult } from './types'
+import { CartLine, FilterQueryResult, FullCartQueryResult, Merchandise, MiniCartQueryResult, MiniProductQueryResult } from './types'
 
 /**
  * Generates an input object from a list of products.
@@ -299,77 +299,6 @@ export const generateFilterQuery = (filter: Filter) => {
     dateAdded:
       activeDateAdded.length > 0 && `(created_at:>${activeDateAdded[0]})`,
   }
-}
-
-/**
- * Cleans up the filter query results
- * @param queryResult The result of passing the filter query
- * @returns A cleaner version of the query result that can be used by components
- */
-export const cleanFilterQueryResult = (
-  queryResult: FilterQueryResult
-): Filter => {
-  const { nodes: productNodes } = queryResult
-  return productNodes.reduce(
-    (acc, cur) => {
-      const removeDup = (list: any[]) => Array.from(new Set(list))
-      const toDefault = (list: any[]) =>
-        list.reduce((acc, cur) => ({ ...acc, [cur]: false }), {})
-      const {
-        options,
-        priceRange,
-        createdAt,
-        collections: { nodes: collectionNodes },
-      } = cur
-      const categoryTitles = [
-        'Airpod',
-        'MacBook',
-        'IPhone',
-        'Watch',
-        'IPad',
-        'Mac',
-      ]
-      const categories = collectionNodes.map((node) => {
-        const title = node.title
-        const idx = categoryTitles.findIndex((categoryTitle) =>
-          title.includes(categoryTitle)
-        )
-        if (idx !== -1) return categoryTitles[idx]
-      })
-      let colors = Array()
-
-      options
-        .filter((option) => option.name === 'Color')
-        .forEach((option) => colors.push(...option.values))
-
-      return {
-        colors: toDefault(removeDup([...Object.keys(acc.colors), ...colors])),
-        dateAdded: toDefault(
-          removeDup([
-            ...Object.keys(acc.dateAdded),
-            new Date(createdAt).getFullYear(),
-          ])
-        ),
-        price: {
-          min: 0,
-          max: 0,
-          tooMax: Math.max(
-            parseInt(priceRange.minVariantPrice.amount),
-            acc.price.tooMax
-          ),
-        },
-        categories: toDefault(
-          removeDup([...Object.keys(acc.categories), ...categories])
-        ),
-      }
-    },
-    {
-      colors: {},
-      dateAdded: {},
-      price: { min: 0, max: 0, tooMax: 0 },
-      categories: {},
-    }
-  )
 }
 
 /**
