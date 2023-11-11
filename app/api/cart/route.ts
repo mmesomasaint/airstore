ccf26be0596ec93169237a5bc782c43a0c6f5cd2
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-11 12:55:52 +0100

    Fix: Format with prettier

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -6,10 +6,16 @@ export async function GET(Request: NextRequest) {
   const searchParams = Request.nextUrl.searchParams
   const cartId = searchParams.get('cartId')
 
-  const variables = {cartId}
-  const {status, body} = await shopifyFetch({query: RETRIEVE_CART_QUERY, variables})
+  const variables = { cartId }
+  const { status, body } = await shopifyFetch({
+    query: RETRIEVE_CART_QUERY,
+    variables,
+  })
 
   if (status === 200) {
-    return Response.json({status: 200, body: cleanFullCartResult(body.data?.cart)})
-  } else return Response.json({status: 500, message: 'Error fetching data'})
+    return Response.json({
+      status: 200,
+      body: cleanFullCartResult(body.data?.cart),
+    })
+  } else return Response.json({ status: 500, message: 'Error fetching data' })
 }
