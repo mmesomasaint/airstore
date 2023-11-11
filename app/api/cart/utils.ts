e459d0dfb13b256396a3a72ee2bbd6777473a2b8
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-11 06:48:38 +0100

    Feat: Design the return interface of fullcart query

--- app/api/cart/utils.ts
+++ app/api/cart/utils.ts
@@ -20,6 +20,61 @@ interface MiniCartQueryResult {
   }
 }
 
+interface FullCartQueryResult {
+  id: string
+  lines: {
+    nodes: {
+      id: string
+      quantity: number
+      merchandise: {
+        id: string
+      }
+      attributes: {
+        key: string
+        value: string
+      }[]
+    }[]
+  }
+  attributes: {
+    key: string
+    value: string
+  }[]
+  cost: {
+    totalAmount: {
+      amount: number
+      currencyCode: string
+    }
+    subtotalAmount: {
+      amount: number
+      currencyCode: string
+    }
+    totalTaxAmount: {
+      amount: number
+      currencyCode: string
+    }
+    totalDutyAmount: {
+      amount: number
+      currencyCode: string
+    }
+  }
+  buyerIdentity: {
+    email: string
+    phone: string
+    customer: {
+      id: string
+    }
+    countryCode: string
+    deliveryAddressPreferences: {
+      address1: string
+      address2: string
+      city: string
+      provinceCode: string
+      countryCodeV2: string
+      zip: string
+    }
+  }
+}
+
 export const CREATE_CART_QUERY = `
 mutation ($input: CartInput) {
   cartCreate(input: $input) {
