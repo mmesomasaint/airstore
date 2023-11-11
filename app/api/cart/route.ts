f3ff47811dd4138fa89dbc55e6a00f9b076cf2ff
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-11 12:33:32 +0100

    Fix: Not needed lines

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -3,7 +3,6 @@ import { NextRequest } from 'next/server'
 export async function GET(Request: NextRequest) {
   const searchParams = Request.nextUrl.searchParams
   const cartId = searchParams.get('cartId')
-  const { cartLines } = await Request.json()
 
   
 }
