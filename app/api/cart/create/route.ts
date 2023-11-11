d579d1797f2893d59e275e3dadcdaacd1ad51a53
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-11 13:21:22 +0100

    Feat: Create the create cart api

---
+++ app/api/cart/create/route.ts
@@ -0,0 +1,17 @@
+import { shopifyFetch } from '@/lib/fetch'
+import {NextRequest} from 'next/server'
+import { CREATE_CART_QUERY, cleanMiniCartResult } from '../utils'
+
+export default async function POST(Request: NextRequest) {
+  const {cartLines} = await Request.json()
+
+  const variables = {cartLines}
+  const {status, body} = await shopifyFetch({query: CREATE_CART_QUERY, variables})
+
+  if (status === 200) {
+    const cart = cleanMiniCartResult(body.data?.cart)
+    return Response.json({status: 200, body: cart})
+  } else {
+    return Response.json({status: 500, message: 'Error receiving data'})
+  }
+}
