601429cb2e4dad127c6ac77a4c1c8d741fa7437d
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-11 13:52:50 +0100

    Feat: Create the update cart api

---
+++ app/api/cart/update/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest } from "next/server";
+import { UPDATE_LINES_QUERY, cleanMiniCartResult, generateCartLinesInput } from "../utils";
+import { shopifyFetch } from "@/lib/fetch";
+
+export default async function POST(Request: NextRequest) {
+  const { cartLines } = await Request.json();
+  const variables = { cartLines: generateCartLinesInput(cartLines) };
+
+  const { status, body } = await shopifyFetch({
+    query: UPDATE_LINES_QUERY,
+    variables,
+  });
+
+  if (status === 200) {
+    const cart = cleanMiniCartResult(body.data?.cart);
+    return Response.json({ status: 200, body: cart });
+  } else {
+    return Response.json({ status: 500, message: "Error receiving data" });
+  }
+}
