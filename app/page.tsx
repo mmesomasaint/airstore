d1a228d5c71ad13aebd7ff8a03b0e7a15aa14ad9
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-13 14:15:39 +0100

    Fix: Format with prettier

--- app/page.tsx
+++ app/page.tsx
@@ -23,9 +23,7 @@ export default function Home() {
         setCategory={setCategory}
         searchClick={searchHandler}
       />
-      <div className='bg-gray-100/70 px-7 py-4 min-h-full grow grid grid-cols-6 gap-9 items-stretch'>
-        
-      </div>
+      <div className='bg-gray-100/70 px-7 py-4 min-h-full grow grid grid-cols-6 gap-9 items-stretch'></div>
     </main>
   )
 }
