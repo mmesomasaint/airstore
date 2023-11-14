0d9676d581dd423440a89b0afd68be187238a711
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-15 00:22:37 +0100

    Feat: Create a new route api for post to handle cursor querys

--- app/api/get/product/all/route.ts
+++ app/api/get/product/all/route.ts
@@ -2,6 +2,7 @@ import { RETRIEVE_ALL_PRODUCTS } from '@/app/api/query'
 import { MiniProductQueryResult } from '@/app/api/types'
 import { cleanMiniProduct } from '@/app/api/utils'
 import { shopifyFetch } from '@/lib/fetch'
+import { NextRequest } from 'next/server'
 
 const LIMIT = 24
 
@@ -17,12 +18,40 @@ export async function GET() {
 
   if (status === 200) {
     const results = body.data?.products.edges
+    const pageInfo = body.data?.products.pageInfo
 
     const cleanedResults = results.map(
       ({ node }: { node: MiniProductQueryResult }) => cleanMiniProduct(node)
     )
 
-    return Response.json({ status, body: cleanedResults })
+    return Response.json({ status, body: {results: cleanedResults, pageInfo} })
+  } else
+    return Response.json({ status: 500, message: 'Error receiving data..' })
+}
+
+export async function POST(Request: NextRequest) {
+  const searchParams = Request.nextUrl.searchParams
+  const cursor = searchParams.get('cursor')
+
+  const variables = {
+    first: LIMIT,
+    cursor
+  }
+
+  const { status, body } = await shopifyFetch({
+    query: RETRIEVE_ALL_PRODUCTS,
+    variables,
+  })
+
+  if (status === 200) {
+    const results = body.data?.products.edges
+    const pageInfo = body.data?.products.pageInfo
+
+    const cleanedResults = results.map(
+      ({ node }: { node: MiniProductQueryResult }) => cleanMiniProduct(node)
+    )
+
+    return Response.json({ status, body: {results: cleanedResults, pageInfo} })
   } else
     return Response.json({ status: 500, message: 'Error receiving data..' })
 }
