3da0fb8e86b6400b8885b85d74772a8e59ab8ad5
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-14 22:22:42 +0100

    Feat: Write the RETREIVE_ALL_PRODUCTS query

--- app/api/query.ts
+++ app/api/query.ts
@@ -307,3 +307,44 @@ query AllProducts($first: Int, $query: String) {
   }
 }
 `
+
+export const RETRIEVE_ALL_PRODUCTS = `
+query AllProducts($first: Int!) {
+  products(first: $first) {
+    edges {
+      node {
+        id
+        title
+        handle
+        featuredImage {
+          url
+        }
+        priceRange {
+          minVariantPrice {
+            amount
+          }
+        }
+        compareAtPriceRange {
+          maxVariantPrice {
+            amount
+          }
+        }
+        options {
+          name
+          values
+        }
+        collections(first: 10) {
+          nodes {
+            handle
+            title
+          }
+        }
+      }
+    }
+    pageInfo {
+      hasNextPage
+      cursor
+    }
+  }
+}
+`
