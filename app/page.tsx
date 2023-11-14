62a8856f567f60d1b6fe59cb5203d73c889795d8
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-15 00:16:07 +0100

    Feat: Create the product state

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { MiniProduct } from '@/lib/product'
 import Header from '@/theme/components/header'
+import Loading from '@/theme/components/loading'
+import { VCard } from '@/theme/components/product/card'
 import useSearch from '@/theme/components/useSearch'
+import Button from '@/theme/elements/button'
+import { useState } from 'react'
 
 export default function Home() {
+  const [hasMore, setHasMore] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [products, setProducts] = useState<MiniProduct[]>([])
   const {
     searchText,
     filter,
@@ -11,6 +19,8 @@ export default function Home() {
     setCategory,
     searchHandler,
   } = useSearch()
+
+  const DisplayLoading = () => (loading && products.length <= 0) && <Loading />
 
   return (
     <main className='min-h-screen flex flex-col'>
@@ -23,7 +33,23 @@ export default function Home() {
         setCategory={setCategory}
         searchClick={searchHandler}
       />
-      <div className='bg-gray-100/70 px-7 py-4 min-h-full grow grid grid-cols-6 gap-9 items-stretch'></div>
+      <div className='bg-gray-100/70 px-7 py-4 min-h-full grow grid grid-cols-6 gap-9 items-stretch'>
+        <DisplayLoading />
+        {products.length > 0 && (
+          products.map((product: MiniProduct, id) => (
+            <VCard
+              key={`${product.src + id}`}
+              title={product.title}
+              handle={product.handle}
+              src={product.src}
+              price={product.price}
+              discount={product.discount}
+              colors={product.colors}
+              collectionHandle={product.collectionHandle}
+            />
+          )))}
+        {hasMore && <Button outlinePrimary>{loading ? 'Loading..' : 'Load More'}</Button>}
+      </div>
     </main>
   )
 }
