fd1eb3a47dda67dc27a4cdc843f3ab65a7eaf8ea
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-14 22:27:59 +0100

    Fix: Format with prettier

--- app/api/get/product/all/route.ts
+++ app/api/get/product/all/route.ts
@@ -1,25 +1,29 @@
-import { RETRIEVE_ALL_PRODUCTS } from "@/app/api/query";
-import { MiniProductQueryResult } from "@/app/api/types";
-import { cleanMiniProduct } from "@/app/api/utils";
-import { shopifyFetch } from "@/lib/fetch";
-import { NextRequest } from "next/server";
+import { RETRIEVE_ALL_PRODUCTS } from '@/app/api/query'
+import { MiniProductQueryResult } from '@/app/api/types'
+import { cleanMiniProduct } from '@/app/api/utils'
+import { shopifyFetch } from '@/lib/fetch'
+import { NextRequest } from 'next/server'
 
 const LIMIT = 24
 
 export async function GET(Request: NextRequest) {
   const variables = {
-    first: LIMIT
+    first: LIMIT,
   }
 
-  const {status, body} = await shopifyFetch({query: RETRIEVE_ALL_PRODUCTS, variables})
+  const { status, body } = await shopifyFetch({
+    query: RETRIEVE_ALL_PRODUCTS,
+    variables,
+  })
 
   if (status === 200) {
     const results = body.data?.products.edges
-    
+
     const cleanedResults = results.map(
       ({ node }: { node: MiniProductQueryResult }) => cleanMiniProduct(node)
     )
 
-    return Response.json({status, body: cleanedResults})
-  } else return Response.json({status: 500, message: 'Error receiving data..'})
+    return Response.json({ status, body: cleanedResults })
+  } else
+    return Response.json({ status: 500, message: 'Error receiving data..' })
 }
