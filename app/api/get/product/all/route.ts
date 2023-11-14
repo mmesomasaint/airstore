f79f5e4970fb872a21f8b57a2dbe162780977c7e
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-14 22:27:51 +0100

    Feat: Complete the all products api design

--- app/api/get/product/all/route.ts
+++ app/api/get/product/all/route.ts
@@ -1,3 +1,7 @@
+import { RETRIEVE_ALL_PRODUCTS } from "@/app/api/query";
+import { MiniProductQueryResult } from "@/app/api/types";
+import { cleanMiniProduct } from "@/app/api/utils";
+import { shopifyFetch } from "@/lib/fetch";
 import { NextRequest } from "next/server";
 
 const LIMIT = 24
@@ -6,4 +10,16 @@ export async function GET(Request: NextRequest) {
   const variables = {
     first: LIMIT
   }
+
+  const {status, body} = await shopifyFetch({query: RETRIEVE_ALL_PRODUCTS, variables})
+
+  if (status === 200) {
+    const results = body.data?.products.edges
+    
+    const cleanedResults = results.map(
+      ({ node }: { node: MiniProductQueryResult }) => cleanMiniProduct(node)
+    )
+
+    return Response.json({status, body: cleanedResults})
+  } else return Response.json({status: 500, message: 'Error receiving data..'})
 }
