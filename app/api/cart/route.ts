c3b00de380c95a53ce6ef9ad6ac366c24d2dd78f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-23 21:17:20 +0100

    Fix: Destructure body properly

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -16,7 +16,7 @@ export async function GET(Request: NextRequest) {
   if (status === 200) {
     return Response.json({
       status: 200,
-      body: cleanFullCartResult(body.data?.cart),
+      body: cleanFullCartResult(body.data?.cartCreate?.cart),
     })
   } else return Response.json({ status: 500, message: 'Error fetching data' })
 }
