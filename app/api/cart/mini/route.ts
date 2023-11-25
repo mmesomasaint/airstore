21631ac1d6b307fdf92144ee9ab41623c28e8461
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-25 21:37:34 +0100

    Feat: Design the mini-cart route api

---
+++ app/api/cart/mini/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest } from "next/server";
+import { RETRIEVE_MINI_CART } from "../../query";
+import { shopifyFetch } from "@/lib/fetch";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const cartId = searchParams.get("cartId");
+
+  const { status, body } = await shopifyFetch({
+    query: RETRIEVE_MINI_CART,
+    variables: { cartId }
+  });
+
+  if (status === 200) {
+    return Response.json({
+      status: 200,
+      body: cleanMiniCartResult(body.data?.cart)
+    });
+  } else {
+    return Response.json({ status: 500, message: "Error fetching data" });
+  }
+}
