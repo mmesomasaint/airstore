28251e5f1195d82e7c7a34acd3083de5a095b8fa
Mmesoma Saint <mmesomasaint@gmail.com>
2023-11-25 21:40:13 +0100

    Fix: Format with prettier

--- app/api/cart/mini/route.ts
+++ app/api/cart/mini/route.ts
@@ -1,23 +1,23 @@
-import { NextRequest } from "next/server";
-import { RETRIEVE_MINI_CART } from "../../query";
-import { shopifyFetch } from "@/lib/fetch";
-import { cleanMiniCartResult } from "../../utils";
+import { NextRequest } from 'next/server'
+import { RETRIEVE_MINI_CART } from '../../query'
+import { shopifyFetch } from '@/lib/fetch'
+import { cleanMiniCartResult } from '../../utils'
 
 export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-  const cartId = searchParams.get("cartId");
+  const searchParams = request.nextUrl.searchParams
+  const cartId = searchParams.get('cartId')
 
   const { status, body } = await shopifyFetch({
     query: RETRIEVE_MINI_CART,
-    variables: { cartId }
-  });
+    variables: { cartId },
+  })
 
   if (status === 200) {
     return Response.json({
       status: 200,
-      body: cleanMiniCartResult(body.data?.cart)
-    });
+      body: cleanMiniCartResult(body.data?.cart),
+    })
   } else {
-    return Response.json({ status: 500, message: "Error fetching data" });
+    return Response.json({ status: 500, message: 'Error fetching data' })
   }
 }
